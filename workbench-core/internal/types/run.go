0874e26bc3914fd2340194f506dcccd26a4260d3
Santino Onyeme <santino.onyeme@hotmail.co.uk>
2026-01-08 11:10:35 +0000

    feat: Add Run and RunStatus types to manage execution state

--- workbench-core/internal/types/run.go
+++ workbench-core/internal/types/run.go
@@ -1,1 +1,15 @@
 package types
+
+import "time"
+
+type RunStatus string
+
+type Run struct {
+	RunId, Goal           string
+	Status                RunStatus
+	StartedAt, FinishedAt time.Time
+	MaxBytesForContext    int
+	Err                 error
+}
+
+
